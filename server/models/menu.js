f965dcb15b38ce143a5a219b10d32503d4a265c3
johnadesina <john.sama3@gmail.com>
2018-04-24 18:07:12 +0200

    To set a menu for the day and to get it

---
+++ server/models/menu.js
@@ -0,0 +1,33 @@
+const Menus =  [
+{
+	menuid: 1,
+	menuName: 'Fasting menu',
+	Date: '20/4/2018',
+	foodOp: [{
+		mealid: 1,
+		mealName: 'carrot with chips',
+		mealPrice: '1000'
+	},
+	{
+		mealid: 2,
+		mealName: 'vegetable with rice',
+        mealPrice: '1500'
+	}]},{
+
+	menuid: 2,
+	menuName: 'consumer menu',
+	Date: '23/4/2018',
+	foodOp: [{
+		mealid: 1,
+		mealName: 'potatoes with turkey',
+		mealPrice: '2000'
+	},
+	{
+		mealid: 2,
+		mealName: 'rice with beef',
+		mealPrice: '2000'
+	}]}
+
+]
+
+export default Menus;
