b3d8b19854bf06098232ab238c1d674212a85512
johnadesina <john.sama3@gmail.com>
2018-04-28 18:52:06 +0200

    set up

--- server/app.js
+++ server/app.js
@@ -30,6 +30,10 @@ app.use('/api/v1/orders', order);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome To Book a Meal API!!!',
+}));
+
 app.listen('3000', () => {
 console.log('Running on port 3000...')
 });
